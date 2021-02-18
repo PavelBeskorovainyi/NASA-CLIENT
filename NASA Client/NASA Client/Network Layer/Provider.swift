4d29dfb201b376ee62a2e63c2a5ab42cff0ff712
PavelBeskorovainyi <beskor1993@icloud.com>
2021-02-18 20:35:37 +0200

    add request models + network layer

---
+++ NASA Client/NASA Client/Network Layer/Provider.swift
@@ -0,0 +1,36 @@
+//
+//  Provider.swift
+//  NASA Client
+//
+//  Created by Павел Бескоровайный on 18.02.2021.
+//
+
+import Foundation
+import PromiseKit
+import Moya
+
+struct Provider {
+    static private let provider = MoyaProvider<RequestTarget>()
+    
+    static func getDataFrom(rover: String, camera: String, date: Date, page: Int) -> Promise<RequestModel> {
+        return Promise { seal in
+            provider.request(.requestWithParametrs(rover: rover, camera: camera, date: date, page: page)) {
+                result in
+                switch result {
+                case.success(let response):
+                    do {
+                        let marsData = try JSONDecoder().decode(RequestModel.self, from: response.data)
+                        seal.fulfill(marsData)
+                    } catch let error {
+                        debugPrint(error.localizedDescription)
+                        seal.reject(error)
+                    }
+                case .failure(let error):
+                    debugPrint(error.localizedDescription)
+                    seal.reject(error)
+                }
+
+            }
+        }
+    }
+}
---
+++ NASA Client/NASA Client/Network Layer/Target.swift
@@ -0,0 +1,55 @@
+//
+//  Target.swift
+//  NASA Client
+//
+//  Created by Павел Бескоровайный on 18.02.2021.
+//
+
+import Foundation
+import Moya
+
+enum RequestTarget {
+    case requestWithParametrs(rover: String, camera: String, date: Date, page: Int)
+}
+
+extension RequestTarget: TargetType {
+    var baseURL: URL {
+        let uRL = URL(string: "https://api.nasa.gov/")
+            return uRL!
+    }
+    
+    var path: String {
+        switch self {
+        case .requestWithParametrs(let rover, _, _, _):
+            return "mars-photos/api/v1/rovers/\(rover)/photos"
+        }
+    }
+    
+    var method: Moya.Method {
+        .get
+    }
+    
+    var sampleData: Data {
+        return Data()
+    }
+    
+    var task: Task {
+        switch self {
+        case .requestWithParametrs(_, let camera, let date, let page):
+            var parameters = [String:Any]()
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "YYYY-MM-DD"
+            let stringFromDate = dateFormatter.string(from: date)
+            
+            parameters["page"] = page
+            parameters["camera"] = camera
+            parameters["earth_date"] = stringFromDate
+            parameters["api_key"] = "orkh6qZtcyqxpfyuQELyMyfFwCUbRAQJf1MvQCLy"
+            return .requestParameters(parameters: parameters, encoding: URLEncoding.default)
+        }
+    }
+    
+    var headers: [String : String]? {
+        return nil
+    }
+}
