3a733321936ea2fef00a06ad57230e7a6f689582
PavelBeskorovainyi <beskor1993@icloud.com>
2021-02-18 19:10:49 +0200

    add request models

--- NASA Client/NASA Client/Network Layer/RealmModel.swift
+++ NASA Client/NASA Client/Network Layer/RealmModel.swift
@@ -6,5 +6,24 @@
 //
 
 import Foundation
+import RealmSwift
 
-
+class RealmRequestModel: Object {
+    @objc dynamic var roverName: String?
+    @objc dynamic var cameraName: String?
+    @objc dynamic var imagePath: String?
+    @objc dynamic var cameraFullName: String?
+    @objc dynamic var dateCreated: String = String(Int(Date.timeIntervalSinceReferenceDate))
+    
+    override class func primaryKey() -> String? {
+      return "dateCreated"
+    }
+    
+    convenience init(from codableModel: Photo) {
+      self.init()
+        self.roverName = codableModel.rover.name
+        self.cameraName = codableModel.camera.name
+        self.imagePath = codableModel.imagePath
+        self.cameraFullName = codableModel.camera.fullName
+    }
+}
--- NASA Client/NASA Client/Network Layer/RequestModel.swift
+++ NASA Client/NASA Client/Network Layer/RequestModel.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+import RealmSwift
+
+struct RequestModel: Codable {
+    var photos: [Photo]
+}
+
+class Photo: Codable {
+    var imagePath: String?
+    var camera: Camera
+    var rover: Rover
+    
+    enum CodingKeys: String, CodingKey {
+        case camera, rover
+        case imagePath = "img_src"
+    }
+    
+    init(from realm: RealmRequestModel) {
+        self.imagePath = realm.imagePath
+        self.camera = Camera(name: realm.cameraName, fullName: realm.cameraFullName)
+        self.rover = Rover(name: realm.roverName)
+    }
+    
+    func putObjectToRealm(){
+        let realm = try! Realm()
+        let realmObject = RealmRequestModel(from: self)
+        try! realm.write({
+            realm.add(realmObject, update: .all)
+        })
+    }
+}
+
+struct Camera: Codable {
+    var name: String?
+    var fullName: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case fullName = "full_name"
+    }
+}
+
+struct Rover: Codable {
+    var name: String?
+}
+
