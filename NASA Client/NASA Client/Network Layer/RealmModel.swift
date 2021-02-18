8c770619759dbcc21136159029c6621930c66f49
PavelBeskorovainyi <beskor1993@icloud.com>
2021-02-18 17:08:02 +0200

    pod update

---
+++ NASA Client/NASA Client/Network Layer/RealmModel.swift
@@ -0,0 +1,10 @@
+//
+//  RealmModel.swift
+//  NASA Client
+//
+//  Created by Павел Бескоровайный on 18.02.2021.
+//
+
+import Foundation
+
+
---
+++ NASA Client/NASA Client/Network Layer/RequestModel.swift
@@ -0,0 +1,8 @@
+//
+//  RequestModel.swift
+//  NASA Client
+//
+//  Created by Павел Бескоровайный on 18.02.2021.
+//
+
+import Foundation
